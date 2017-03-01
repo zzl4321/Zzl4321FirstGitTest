ee5807ac0d67820e0f3c973fa9f2eac69a91f5ad
zzl4321 <zzl4321@163.com>
2017-03-01 12:59:40 +0800

    Initial Commit

---
+++ TestGitThird/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGitThird
+//
+//  Created by 郑泽龙 on 2017/3/1.
+//  Copyright © 2017年 郑泽龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
